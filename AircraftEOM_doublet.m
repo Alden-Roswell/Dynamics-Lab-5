99f15736d3019dc301435b74c537bafdcca79c7e
ronald curry <125322497+ronald-curry@users.noreply.github.com>
2026-04-07 09:46:55 -0600

    Update AircraftEOM_doublet.m

--- AircraftEOM_doublet.m
+++ AircraftEOM_doublet.m
@@ -1,15 +1,11 @@
 
 function xdot = AircraftEOM_doublet(time, aircraft_state, aircraft_surfaces, doublet_size,doublet_time, wind_inertial, aircraft_parameters)
     %% Doublet
-    delta_e_t=aircraft_state(1);
     if time<doublet_time
-        delta_e=delta_e_t+doublet_size;
+        aircraft_surfaces(1)=aircraft_surfaces(1)+doublet_size;
     elseif time < 2*doublet_time
-        delta_e=delta_e_t-doublet_size;
-    else
-        delta_e=delta_e_t;
+        aircraft_surfaces(1)=aircraft_surfaces(1)-doublet_size;
     end
-    aircraft_state(1)=delta_e;
 
     xdot=AircraftEOM(time, aircraft_state, aircraft_surfaces, wind_inertial, aircraft_parameters);
 
