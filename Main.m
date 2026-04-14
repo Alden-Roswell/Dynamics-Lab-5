fdc9921e9f4f42c373641855cd2f46b15dbdda7d
Alden-Roswell <alro4526@colorado.edu>
2026-04-14 09:19:11 -0600

    working more plots

--- Main.m
+++ Main.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 clear;
 close all;
 clc;
@@ -49,21 +48,4 @@ fig_21 = 7:1:12
 col = ["r", "asd2"]
 PlotAircraftSim(t_21,X_21', control_array_21,fig_21, col)
 axis equal
-=======
-clear;
-close all;
-clc;
 
-
-run('ttwistor.m');
-aircraft_surfaces = [0,0,0,0]';
-wind_intertial = [0,0,0]';
-y0 = [0 0 -1609.4 0 0 0 21 0 0 0 0 0]';
-tspan = [0,100];
-ode = @(time, aircraft_state) AircraftEOM(time, aircraft_state, aircraft_surfaces, wind_intertial, aircraft_parameters);
-[t,X] = ode45(ode,tspan,y0);
-control_array = zeros(4, length(X));
-fig = 1:1:6
-col = ["b", "asd"]
-PlotAircraftSim(t,X', control_array,fig, col)
->>>>>>> ee3fe1432b21c1bcb50e83e78077de5a355df5d6
